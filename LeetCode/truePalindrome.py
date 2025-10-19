0f36488dc22e7af2b62df4a862da6b3cbd624999
Lingaombe <mngolihannah@gmail.com>
2025-10-19 23:39:14 +0530

    truePalindrome

---
+++ LeetCode/truePalindrome.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        s = s.lower()
+        s = [i for i in s if i.isalpha() == True]
+        s = " ".join(s)
+        return s == s[::-1]
+        s = "race a car"
