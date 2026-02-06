21c020b72a515f4569936fcf0c69af5d0f622ffe
Lingaombe <mngolihannah@gmail.com>
2026-02-06 20:08:17 +0530

    Add files via upload

---
+++ Flask/studManagement.py
@@ -0,0 +1,77 @@
+from flask import Flask,request, redirect, url_for, flash,render_template
+from flask_mysql_connector import MySQL
+
+app = Flask(__name__)
+
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'mysql'
+app.config['MYSQL_DB'] = 'dbhan'
+
+mysql = MySQL(app)
+app.secret_key = "abc"
+
+
+@app.route('/')
+def index():
+    cursor = mysql.connection.cursor()
+    query = "SELECT * FROM dbhan.studentData"
+    cursor.execute(query)
+    data=cursor.fetchall()
+    #return str(data)
+    #cursor.close()
+    return render_template('b6index.html', student=data)
+    
+
+@app.route('/add_student', methods =['POST'])
+def add_student():
+    if request.method=='POST':
+        sid = request.form['sid']
+        name = request.form['name']
+        email = request.form['email']
+        cursor = mysql.connection.cursor()
+        query='INSERT INTO dbhan.studentData (rno,name,email) VALUES (%s, %s, %s)'
+        val=(sid, name, email )
+        cursor.execute(query,val)
+        mysql.connection.commit()
+        flash('Student added successufully')
+        return redirect(url_for('index'))
+ 
+@app.route('/edit/<sid>', methods = ['POST', 'GET'])
+def get_student(sid):
+    cursor = mysql.connection.cursor()
+    query='SELECT * FROM dbhan.studentData WHERE rno = %s'
+    cursor.execute(query,(sid,))
+    #cursor.execute('SELECT * FROM db.student1 WHERE sid = %s', (sid))
+    data = cursor.fetchall()
+    cursor.close()
+    print(data[0])
+    return render_template('b6edit.html', student = data[0])
+ 
+@app.route('/update/<nid>', methods=['POST'])
+def update_student(nid):
+    if request.method == 'POST':
+        #sid = request.form['sid']
+        name = request.form['name']
+        email = request.form['email']
+        cursor = mysql.connection.cursor()
+        query='UPDATE dbhan.studentData SET name=%s, email=%s where rno=%s'
+        cursor.execute(query,(name,email,nid))
+        flash('Student Updated Successfully')
+        mysql.connection.commit()
+        return redirect(url_for('index'))
+
+@app.route('/delete/<nid>', methods = ['POST','GET'])
+def delete_student(nid):
+    cursor = mysql.connection.cursor()
+    cursor.execute('DELETE FROM dbhan.studentData WHERE rno = {0}'.format(nid))
+    #query='DELETE FROM db.student1 WHERE sid = %s'
+    #cursor.execute(query,(nid))
+    mysql.connection.commit()
+    flash('Student Removed Successfully')
+    return redirect(url_for('index'))
+    
+# running application 
+if __name__ == '__main__': 
+    app.run(debug=True) 
